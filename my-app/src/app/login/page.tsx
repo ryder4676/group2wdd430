3d625cb0795308962179b229487bb7077d3c6e28
ryder4676 <60828482+ryder4676@users.noreply.github.com>
2024-02-02 10:54:11 -0700

    login, and register works again but with prisma

---
+++ my-app/src/app/login/page.tsx
@@ -0,0 +1,24 @@
+import Header from "../ui/dashboard/header"
+import Footer from "../ui/dashboard/footer"
+import styles from './login.module.css'
+
+export default function Login(){
+    return (
+        <>
+        <Header/>
+        {/* dkfjdkfjd */}
+        <main className={styles.main}>
+            <h1>Login / Sign Up</h1>
+        <form className={styles.form}>
+            <label className={styles.label}>Email
+                <input className={styles.input} type="text" />
+            </label>
+            <label className={styles.label}>Password
+                <input className={styles.input} type="text" />
+            </label>
+        </form>
+        </main>
+        <Footer/>
+        </>
+    )
+}
