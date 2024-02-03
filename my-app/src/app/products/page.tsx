85e2d5525e2172a88b4eefb874f02246651cb1ce
ailenrmansilla <ailen.r.mansilla@gmail.com>
2024-02-03 03:41:38 -0500

    Created seller's profile page.tsx, also pages to add and edit products. Erased not used files.

---
+++ my-app/src/app/products/page.tsx
@@ -0,0 +1,2 @@
+// we'll render all of the products from all of the sellers here
+// We can add options to filter or organized the products
--- my-app/src/app/ui/product-grid.tsx
+++ my-app/src/app/ui/product-grid.tsx
@@ -1,10 +1,32 @@
-import fetchProductsById from '/';
+import fetchProductsById, fetchAllProducts from '/';
 
 export default async function ProductGrid( {id}:{id: string} ){
     try {
-        const products = await fetchProductsById(id); // Fetch products using id
+        const products = await fetchProductsById(id); // create a function to fetch products using id
         } catch (error) {
     // display an error message or redirect
+    console.error('Error fetching products:', error);
+    }
+
+    return(
+        <div className='craft-section'>
+            <h1>Crafts on sale</h1>
+        {/* Work on this when we know what product information we have */}
+            {/* <div className='products-grid'>
+                {products.map((product) => {
+                    return(
+                        ''
+                    );
+                }
+                )}
+            </div> */}
+        </div>
+    )}
+
+export async function AllProductsGrid(){
+    try {
+        const products = await fetchAllProductsById(); // create a function to fetch products using id
+        } catch (error) {
     console.error('Error fetching products:', error);
     }
 
