c2bdd27e47e22d477ee79115d24cf061e3772d44
ailenrmansilla <ailen.r.mansilla@gmail.com>
2024-02-04 01:22:12 -0500

    form to add new product and related components. Fixing errors.

---
+++ my-app/src/app/seller-profile/add-products/page.tsx
@@ -0,0 +1,10 @@
+import Form from '@/app/ui/create-product-form';
+
+export default async function Page(){
+    return(
+        <main>
+            <Form id='1'/>
+            {/* we need to also send the real seller id here */}
+        </main>
+    )
+}
